cbdece01f81f23a673a802b1f15326ccb5ef63b2
Ingo Karkat <swdev@ingo-karkat.de>
2024-12-10 11:14:08 +0100

    Consistency: Add alias support for commands taking COMMAND(s)
    System-info: Ubuntu 20.04.6 LTS, x86_64
    Platform-info: bash 5.0.17(1)-release, Bats 1.11.0

---
+++ shell/aliases.sh
@@ -0,0 +1,11 @@
+#!/bin/sh source-this-script
+
+addAliasSupport greyerr
+addAliasSupport hlerr
+addAliasSupport firstRestLines
+addAliasSupport insert
+addAliasSupport pipeappend
+addAliasSupport prefix
+addAliasSupport suffix
+addAliasSupport sumerr
+addAliasSupport surround
